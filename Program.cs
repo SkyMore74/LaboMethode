35b67557d918b677011d629c1676a9a7e8d78985
Frédérick <frederickcrunch@gmail.com>
2022-09-30 14:56:31 -0400

    Première Modification

--- Program.cs
+++ Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int choix = ChoisirOptionMenu();
+
+            Console.WriteLine("Vous avez choisi l'option " + choix);
+        }
+
+        static int ChoisirOptionMenu()
+        {
+            int choix=0;
+            string menu = "Choisir 1 pour Jeu du Pendu, 2 pour un Tic Tac Toe, 3 pour quitter";
+            bool valide=false;
+
+            while(!valide)
+            {
+                Console.Clear();
+                Console.WriteLine(menu);
+                int.TryParse(Console.ReadLine(), out choix);
+
+                if(choix == 1 || choix == 2 || choix == 3)
+                {
+                    valide = true;
+                }
+            }
+
+
+            return choix;
         }
     }
 }
